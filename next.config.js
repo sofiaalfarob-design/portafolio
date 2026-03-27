17283ab0f7943786ee9ab4171b11b4f97d2d08a5
JEAN98 <jeancarlovegabejarano@hotmail.com>
2026-03-27 14:11:50 -0600

    github deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/portafolio" : "",
   images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 31536000,
-    remotePatterns: [],
+    unoptimized: true,
   },
 };
 
